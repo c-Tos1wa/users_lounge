641d1d75bd758570aaa11fe128223b9fb8eec692
Cristina <cristoshiwassaki@gmail.com>
2022-04-26 22:12:58 -0300

    iniciando método post

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from '../models/user';
 
 @Injectable({
@@ -8,9 +8,19 @@ import { User } from '../models/user';
 })
 export class UserService {
   api = 'https://sheet.best/api/sheets/5d92918d-007d-41cf-97c4-8aadbede74b3'
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
+
   constructor(private httpClient: HttpClient) { }
 
   getUserList():Observable<User[]>{
     return this.httpClient.get<User[]>(this.api);
+  }
+
+  postUser(user: User):Observable<User>{
+    return this.httpClient.post<User>(this.api, user, this.httpOptions)
   }
 }
